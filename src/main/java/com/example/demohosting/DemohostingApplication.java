288392dd379416942d111f705e29e1118ea5fada
panha.teng@ubill24.com <panha.teng@ubill24.com>
2024-02-09 00:47:54 +0700

    init

---
+++ src/main/java/com/example/demohosting/DemohostingApplication.java
@@ -0,0 +1,13 @@
+package com.example.demohosting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemohostingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemohostingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demohosting/DemohostingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demohosting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemohostingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
